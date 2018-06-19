801cbb1e7bf0d810bd5be5821827fd196901eab3
AntiTriki <ne.i.on@hotmail.com>
2018-06-19 15:50:34 -0400

    fdgfdg
    cgvfdxg

---
+++ conexion.php
@@ -0,0 +1,18 @@
+<?php
+// Conectando, seleccionando la base de datos
+$mysql_host='localhost';
+$mysql_user='root';
+$mysql_password='';
+$my_database='n';
+
+$cxn = new mysqli($mysql_host, $mysql_user, $mysql_password, $my_database);
+if ($cxn->connect_errno) {
+    echo "Fallo al conectar a MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
+}else{
+
+
+}
+
+// Cerrar la conexión
+$cxn->close();
+?>
